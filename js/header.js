405b1dd921ffb7e4de6017aa6afe72af014b8735
Eva <evafhaugum@hotmail.com>
2021-12-21 10:08:37 +0100

    h

---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.write('<div class="topnav" id="myTopnav">');
+document.write('    <a href="index.html" >Hjem</a>');
+document.write('    <a href="berg_tjenester.html">Tjenester</a>');
+document.write('    <a href="index.html" ><img src="./image/logo_nb.png" alt="Logo"></a>');
+document.write('    <a href="berg_kontakt.html">Kontakt</a>');
+document.write('    <a href="berg_om_oss.html">Om oss</a>');
+document.write('    <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>');
+document.write('  </div>');
